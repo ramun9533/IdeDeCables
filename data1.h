721799438ae0761b89711ae1c0df890f1c751430
William Elí O.L <ramun346416@gmail.com>
2023-08-15 15:56:51 +0000

    Update data1.h
    Se hacen adecuaciones para el codigo JS

--- data1.h
+++ data1.h
@@ -1,18 +1,35 @@
-//Toma un parámetro de tipo referencia a "WiFiClient", es decir "WiFiClient &client".
-// El "&" en "WiFiClient" & indica que "client" es una referencia al objeto "WiFiClient". 
-// asi ya no se tiene que hacer un nuevo objeto .
-// En otras palabras, la función puede modificar el objeto original y los cambios se 
-// reflejarán fuera de la función.
-void web(WiFiClient &client){
-   for (int n=1; n<7; n++)
-            {
-            client.print(responseHTML1);
-            client.print(n);
-            client.print(responseHTML2);
-            client.print(n);
-            client.print(responseHTML3);
-            client.print(n);
-            client.print(responseHTML4);
-            }
-            client.print(responseHTML5);
-  }
+  //Toma un parámetro de tipo referencia a "WiFiClient", es decir "WiFiClient &client".
+  // El "&" en "WiFiClient" & indica que "client" es una referencia al objeto "WiFiClient". 
+  // asi ya no se tiene que hacer un nuevo objeto .
+  // En otras palabras, la función puede modificar el objeto original y los cambios se 
+  // reflejarán fuera de la función.
+  void web(WiFiClient &client)
+    {
+     
+     client.print(pagina);
+     for (int n=1; n<7; n++)
+        {
+              client.print(pagina1);
+              client.print(n);
+              client.print(pagina2);
+              client.print(n);
+              client.print(pagina3);
+              client.print(n);
+              client.print(pagina4);
+        }
+     client.print(pagina5);
+     for (int i=1; i<7; i++)
+        {
+              client.print(pagina6);
+              client.print(i);
+              client.print(pagina7);
+              client.print(i);
+              client.print(pagina8);
+              client.print(i);
+              client.print(pagina9);
+              client.print(i);
+              client.print(pagina10);
+        }
+     client.print(pagina11);
+    }
+    
